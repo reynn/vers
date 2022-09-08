49d22af8534c1bd3402ea0f0d22d3cc2a7159226
Nic Patterson <nic@reynn.dev>
2022-09-07 19:30:51 -0700

    format

--- src/bin/vers.rs
+++ src/bin/vers.rs
@@ -13,7 +13,9 @@ use {
 async fn main() -> vers::Result<()> {
     let opts = cli::Opts::default();
 
-    env_logger::builder().filter_level(opts.verbose.log_level_filter()).try_init()?;
+    env_logger::builder()
+        .filter_level(opts.verbose.log_level_filter())
+        .try_init()?;
 
     let config_dir: std::path::PathBuf = if let Some(dir) = opts.data_dir {
         dir
